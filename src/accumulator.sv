a70b3ce1842be341b4e1880d0755498b659c2aff
Emmett Roberts <etrnths16@gmail.com>
2026-04-07 13:47:07 -0400

    added accumulator and freq_divider
    Accumulator links PFD output to DCO input using a control word (6-bit).
    freq_divider takes the DCO clock output and divides the frequency by 8 for the PFD feedback clock input

---
+++ src/accumulator.sv
@@ -0,0 +1,24 @@
+module accumulator (
+    input logic up,
+    input logic down,
+    input logic clk,
+    input logic reset; // active low
+    output logic [5:0] controlWord
+);
+
+
+always_ff @(posedge clk or negedge reset) begin
+    if (!reset) begin
+        // lowest possible value means fastest possible clock for fastest 
+        // sampling for quickest convergence to locked loop 
+        controlWord <= 6'b0;
+    end else begin 
+        if (up && !down && controlWord != 6'b0) begin
+            // feedback is behind reference, speed up (subtract transistors)
+            controlWord <= controlWord - 1;
+        end else if (down && !up && controlWord != 6'b63) begin
+            // feedback is ahead of reference, slow down (add transistors)
+            controlWord <= controlWord + 1;
+        end 
+    end 
+end
