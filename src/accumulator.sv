c326f89fe7740d8325841fe1d52b9d5fa2ca7047
Emmett Roberts <etrnths16@gmail.com>
2026-04-30 15:06:02 -0400

    updated accumulator.sv
    accum

--- src/accumulator.sv
+++ src/accumulator.sv
@@ -2,23 +2,65 @@ module accumulator (
     input logic up,
     input logic down,
     input logic clk,
-    input logic reset; // active low
-    output logic [5:0] controlWord
+    input logic rstn, // active low
+    output logic [3:0] controlWord,
+    output logic nand1,
+    output logic nand2,
+    output logic nand3
 );
 
+logic [6:0] sum;
 
-always_ff @(posedge clk or negedge reset) begin
-    if (!reset) begin
+// need to incorperate another output for the nand4 or somehow run the nand4 off
+// same controlWord
+always_ff @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
         // lowest possible value means fastest possible clock for fastest 
         // sampling for quickest convergence to locked loop 
-        controlWord <= 6'b0;
+        controlWord <= 4'b0;
+        nand1 <= 1'b0;
+        nand2 <= 1'b0;
+        nand3 <= 1'b0;
+        sum <= 7'b0;
     end else begin 
-        if (up && !down && controlWord != 6'b0) begin
+        if (up && !down && sum != 6'b0) begin
             // feedback is behind reference, speed up (subtract transistors)
-            controlWord <= controlWord - 1;
-        end else if (down && !up && controlWord != 6'b63) begin
+            sum <= sum - 1;
+        end else if (down && !up && sum != 6'b63) begin
             // feedback is ahead of reference, slow down (add transistors)
-            controlWord <= controlWord + 1;
+            sum <= sum + 1;
         end 
+
+        // this creates 3 inputs for our frequency fine tuning by driving delay
+        // of ring oscillator with nand4 variable capacitance
+
+        // Potential Problem: There are dead spots between 1, 2, and 3 nand4
+        // inputs turning on (could lower so they occur sequentially with only 2
+        // LSB)
+        if (sum[0:2] > 3'd0) begin
+            // if the control word's 3 LSBs are greater than 0 (001, 010, 011,
+            // 100, etc) turn on one of the NAND4 inputs
+            nand1 <= 1'b1;
+        end else begin
+            nand1 <= 1'b0;
+        end
+        if (sum[0:2] > 3'd1) begin
+            // if 3 LSBs are greater than 1 (second one turns on if need more
+            // than just the first)
+            nand2 <= 1'b1;
+        end else begin
+            nand2 <= 1'b0;
+        end
+        if (sum[0:2] > 3'd3) begin
+            // if 3 LSBs are greater than 011 (first two on) then turn on third one
+            nand3 <= 1'b1;
+        end else begin
+            nand3 <= 1'b0;
+        end
     end 
 end
+
+// course adjustsments (# of inverters) get the 4 MSBs
+assign control = sum[3:6];
+
+endmodule
